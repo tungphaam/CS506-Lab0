392b0b9574a7465feedf74766a34a0ddd6f28428
w3cj <cj@null.computer>
2024-09-12 15:46:36 -0400

    Code Updated

--- test_utils.py
+++ test_utils.py
@@ -14,18 +14,22 @@ def test_dot_product():
     
 def test_cosine_similarity():
     ### YOUR CODE HERE
+    vector1 = np.array([1, 2, 3])
+    vector2 = np.array([4, 5, 6])
+
+    result = cosine_similarity(vector1, vector2)
     
-    result = ### YOUR CODE HERE
-    
-    expected_result = ### YOUR CODE HERE
+    expected_result = np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
     
     assert np.isclose(result, expected_result), f"Expected {expected_result}, but got {result}"
 
 def test_nearest_neighbor():
     ### YOUR CODE HERE
+    target_vector = np.array([1, 2])
+    vectors = np.array([[1, 1], [2, 2], [3, 3]])
+
+    result = nearest_neighbor(target_vector, vectors)
     
-    result = ### YOUR CODE HERE
-    
-    expected_index = ### YOUR CODE HERE
+    expected_index = 0
     
     assert result == expected_index, f"Expected index {expected_index}, but got {result}"
--- utils.py
+++ utils.py
@@ -9,7 +9,8 @@ def dot_product(v1, v2):
     # Hint: use `np.dot`.
     '''
     ### YOUR CODE HERE
-    
+    return np.dot(v1, v2)
+
 def cosine_similarity(v1, v2):
     '''
     v1 and v2 are vectors of same shape.
@@ -27,6 +28,7 @@ def cosine_similarity(v1, v2):
     # Hint: Use `dot_product` and `np.linalg.norm`.
     '''
     ### YOUR CODE HERE
+    return dot_product(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
     
 def nearest_neighbor(target_vector, vectors):
     '''
@@ -39,3 +41,13 @@ def nearest_neighbor(target_vector, vectors):
     # Hint: For this lab, you can just use a for loop to iterate through vectors.
     '''
     ### YOUR CODE HERE
+    best_index = -1
+    best_similarity = -1
+
+    for i, vec in enumerate(vectors):
+        similarity = cosine_similarity(target_vector, vec)
+        if similarity > best_similarity:
+            best_similarity = similarity
+            best_index = i
+
+    return best_index
